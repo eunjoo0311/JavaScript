e94648527840f3ebab686039a3c0267c62dbc49e
eunjoo0311 <yyy7423@naver.com>
2023-04-24 17:58:57 +0900

    Add:0424study

--- JavaScript/localStorage/script.js
+++ JavaScript/localStorage/script.js
@@ -1,1 +1,16 @@
+/*
+const myName='은주'
+localStorage.setItem('myName',myName)
+localStorage.setItem('cat',고양이)
+localStorage.setItem('dog',강아지)
+localStorage.setItem('myName','이은주') // 겹치면 덮어씀
+
+하나 삭제
+locatStroage.removeItem('dog')
+
+전부 삭제
 localStorage.clear();
+*/
+const myName = "은주";
+localStorage.setItem("myName", myName);
+localStorage.setItem("cat", "고양이");
